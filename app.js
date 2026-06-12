1a60b1caba17b12dd1291839b5e90fe521014805
Sergio Sainz <100535668+sergiolplp@users.noreply.github.com>
2026-06-12 19:47:57 -0300

    Add functionality to switch screens on button click

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+const screens = document.querySelectorAll(".screen");
 
+function mostrar(id){
+
+  screens.forEach(screen=>{
+    screen.classList.remove("active");
+  });
+
+  document
+  .getElementById(id)
+  .classList.add("active");
+}
+
+document
+.getElementById("btnResultados")
+.onclick=()=>mostrar("resultados");
+
+document
+.getElementById("btnPosiciones")
+.onclick=()=>mostrar("posiciones");
+
+document
+.getElementById("btnEstadisticas")
+.onclick=()=>mostrar("estadisticas");
+
+document
+.querySelectorAll(".volver")
+.forEach(btn=>{
+
+  btn.onclick=()=>mostrar("inicio");
+
+});
