4db572d66fe7e67eb51a69d9f48be851fe3d6957
Sergio Sainz <100535668+sergiolplp@users.noreply.github.com>
2026-06-12 20:02:16 -0300

    Add partidos.js with match data

---
+++ partidos.js
@@ -0,0 +1,10 @@
+const partidos = [
+  { id:1, local:"MÉXICO", visitante:"SUDÁFRICA", r1:"", r2:"" },
+  { id:2, local:"COREA del SUR", visitante:"REP. CHECA", r1:"", r2:"" },
+  { id:3, local:"CANADÁ", visitante:"BOSNIA Y HERZEG.", r1:"", r2:"" },
+  { id:4, local:"ESTADOS UNIDOS", visitante:"PARAGUAY", r1:"", r2:"" },
+  { id:5, local:"CATAR", visitante:"SUIZA", r1:"", r2:"" },
+  { id:6, local:"BRASIL", visitante:"MARRUECOS", r1:"", r2:"" },
+  { id:7, local:"HAITÍ", visitante:"ESCOCIA", r1:"", r2:"" },
+  { id:8, local:"AUSTRALIA", visitante:"TURQUÍA", r1:"", r2:"" }
+];
